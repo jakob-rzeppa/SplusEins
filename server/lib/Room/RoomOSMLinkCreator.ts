1c6c406db38341a84b2ad63d6faafa0e2ab45545
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-12-27 15:42:47 +0100

    update RoomOSMLinkCreator.ts to use the osm id data for each location

--- server/lib/Room/RoomOSMLinkCreator.ts
+++ server/lib/Room/RoomOSMLinkCreator.ts
@@ -1,4 +1,7 @@
-import * as OSM_ROOMS_DATA from '../../assets/overpassOSMRoomsData.json';
+import * as OSM_ROOMS_DATA_WF from '../../assets/overpass_osm/WF.json';
+import * as OSM_ROOMS_DATA_SZ from '../../assets/overpass_osm/SZ.json';
+import * as OSM_ROOMS_DATA_WOB from '../../assets/overpass_osm/WOB.json';
+import * as OSM_ROOMS_DATA_SUD from '../../assets/overpass_osm/SUD.json';
 
 import { FacultyLocation } from './RoomLocationApi';
 
@@ -9,22 +12,29 @@ import { FacultyLocation } from './RoomLocationApi';
  * @returns OSM link for the room or null if not available
  */
 export function createOSMLink(room: string, facultyLocation: FacultyLocation): string | null {
+  let osmData: { id: number, level: string } | null = null;
+
+  // Select the correct OSM data based on the faculty location
   switch (facultyLocation) {
     case 'WF':
-      return createOSMLinkWolfenbuettel(room);
-    default:
+      osmData = OSM_ROOMS_DATA_WF[room];
+      break;
+    case 'SZ':
+      osmData = OSM_ROOMS_DATA_SZ[room];
+      break;
+    case 'WOB':
+      osmData = OSM_ROOMS_DATA_WOB[room];
+      break;
+    case 'SUD':
+      osmData = OSM_ROOMS_DATA_SUD[room];
       break;
   }
-  return null;
-}
 
-function createOSMLinkWolfenbuettel(room: string): string | null {
   /**
-   * Check if we have OSM data for this room in Wolfenbüttel
-   * If so, add a link to the OSM way
+   * Check if we have OSM data for this room in the selected location
+   * If we do, create the OSM link using the ID
    * Example: Link: https://osmapp.org/way/123456789
    */
-  const osmData = OSM_ROOMS_DATA[room];
   if (osmData) {
     return `https://osmapp.org/way/${osmData.id}`;
   }
--- server/tests/lib/Room/RoomOSMLinkCreator.test.ts
+++ server/tests/lib/Room/RoomOSMLinkCreator.test.ts
@@ -1,33 +1,67 @@
 // Mock OSM data
-jest.mock('../../../assets/overpassOSMRoomsData.json', () => ({
-  "WF-EX-2/252": { id: 123456789, level: "2" },
-  "WF-EX-3/101": { id: 987654321, level: "1" },
-  "A068": { id: 123456789, level: "0" }
+jest.mock('../../../assets/overpass_osm/WF.json', () => ({
+  "test-room": { id: 11111111, level: "1" }
+}), { virtual: true });
+jest.mock('../../../assets/overpass_osm/SZ.json', () => ({
+  "test-room": { id: 22222222, level: "1" }
+}), { virtual: true });
+jest.mock('../../../assets/overpass_osm/SUD.json', () => ({
+  "test-room": { id: 333333333, level: "1" }
+}), { virtual: true });
+jest.mock('../../../assets/overpass_osm/WOB.json', () => ({
+  "test-room": { id: 444444444, level: "1" }
 }), { virtual: true });
 
 describe('RoomOSMLinkCreator', () => {
   describe('WF', () => {
-    it('should return correct OSM link for WF-EX-2/252 in WF faculty', () => {
+    it('should return correct OSM link for test-room in WF faculty', () => {
       const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
-      const link = createOSMLink('WF-EX-2/252', 'WF');
-      expect(link).toBe('https://osmapp.org/way/123456789');
-    });
-
-    it('should return correct OSM link for WF-EX-3/101 in WF faculty', () => {
-      const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
-      const link = createOSMLink('WF-EX-3/101', 'WF');
-      expect(link).toBe('https://osmapp.org/way/987654321');
-    });
-
-    it('should return correct OSM link for A068 in WF faculty', () => {
-      const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
-      const link = createOSMLink('A068', 'WF');
-      expect(link).toBe('https://osmapp.org/way/123456789');
+      const link = createOSMLink('test-room', 'WF');
+      expect(link).toBe('https://osmapp.org/way/11111111');
     });
 
     it('should return null for unknown room in WF faculty', () => {
       const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
       const link = createOSMLink('UNKNOWN_ROOM', 'WF');
+      expect(link).toBeNull();
+    });
+  })
+  describe('SZ', () => {
+    it('should return correct OSM link for test-room in SZ faculty', () => {
+      const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
+      const link = createOSMLink('test-room', 'SZ');
+      expect(link).toBe('https://osmapp.org/way/22222222');
+    });
+
+    it('should return null for unknown room in SZ faculty', () => {
+      const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
+      const link = createOSMLink('UNKNOWN_ROOM', 'SZ');
+      expect(link).toBeNull();
+    });
+  })
+  describe('SUD', () => {
+    it('should return correct OSM link for test-room in SUD faculty', () => {
+      const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
+      const link = createOSMLink('test-room', 'SUD');
+      expect(link).toBe('https://osmapp.org/way/333333333');
+    });
+
+    it('should return null for unknown room in SUD faculty', () => {
+      const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
+      const link = createOSMLink('UNKNOWN_ROOM', 'SUD');
+      expect(link).toBeNull();
+    });
+  })
+  describe('WOB', () => {
+    it('should return correct OSM link for test-room in WOB faculty', () => {
+      const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
+      const link = createOSMLink('test-room', 'WOB');
+      expect(link).toBe('https://osmapp.org/way/444444444');
+    });
+
+    it('should return null for unknown room in WOB faculty', () => {
+      const { createOSMLink } = require("../../../lib/Room/RoomOSMLinkCreator");
+      const link = createOSMLink('UNKNOWN_ROOM', 'WOB');
       expect(link).toBeNull();
     });
   })
